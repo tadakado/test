d85a58d9ab15245054412dcb401d1119670ee79b
Tadashi Kadowaki <tadakado@gmail.com>
2020-02-06 22:12:10 +0900

    update

--- bleuart.ts
+++ bleuart.ts
@@ -1,5 +1,9 @@
 //% color=#f44242 icon="\uf185"
 namespace BLEUart {
+    export function start() {
+        BLEUart.startService()
+    }
+
     //% shim=BLEUart::startService
     function startService() {
         return 0
